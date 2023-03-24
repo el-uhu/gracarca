d777ec6022d73b69762901d7649d5033e0b23559
Lukas Hutter <luki@biotop.co>
2023-03-24 16:03:23 +0000

    finally fixed the bug of the model not working properly!!!

--- src/components/gracarca/Gracarca.MapModel.svelte
+++ src/components/gracarca/Gracarca.MapModel.svelte
@@ -1,39 +1,24 @@
 <script>
-	import { Canvas, AmbientLight, OrbitControls, PerspectiveCamera } from '@threlte/core'
+	import { Canvas } from '@threlte/core'
+    import { T, OrbitControls } from '@threlte/core'
 	import { GLTF, useGltfAnimations } from '@threlte/extras'
+    import { spring } from 'svelte/motion'
+	import { degToRad } from 'three/src/math/MathUtils'
 
-    let pos = {
-        x: -275,
-        y: 1750,
-        z: -50
-    };
-
-    let rot = {
-        x: -270,
-        y: 0,
-        z: 0
-    };
+    let pos = [-165, 635, 1084]
+    let rot = [-270, 0, 0]
+    export let camera
+    $: console.log(camera);
 
 </script>
 
 <div class="canvascontainer">
 	<Canvas>
-        <!-- <Environment path="/hdr/" files="shanghai_riverside_1k.hdr" /> -->
-
-        <PerspectiveCamera 
-            position={pos},
-            rotation={rot},
-            fov={75},
-            near={0.01},
-            far={10000}>
-            <OrbitControls enableDamping />
-        </PerspectiveCamera>
+        <T.PerspectiveCamera bind:ref={camera} makeDefault position={pos} rotation={rot} fov={75} far={10000}>
+            <OrbitControls maxPolarAngle={degToRad(80)} enableZoom={true}} />
+        </T.PerspectiveCamera>
         
-        <AmbientLight />
-        
-        <GLTF 
-            url="/models/Gracarca_SkyView_Weg_Tafeln_2023-03-22.gltf"
-            useDraco="true" />
+        <GLTF url="/models/Gracarca_SkyView_Weg_Tafeln_2023-03-22.gltf"/>
 	</Canvas>
 </div>
 
