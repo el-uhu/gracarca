e17fd2923da5e594bb348b47acadc431bb7e18b2
Lukas Hutter <el-uhu@users.noreply.github.com>
2023-10-11 20:32:14 +0000

    troubleshooting vercel

--- svelte.config.js
+++ svelte.config.js
@@ -27,6 +27,39 @@ const config = {
 		}), 
 		preprocessThrelte()]),
 	kit: {
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: null,
+			precompress: false,
+			strict: false
+		}),
+		prerender : {
+			handleHttpError: 'warn',
+			entries: [
+				'/de/01',
+				'/de/02',
+				'/de/03',
+				'/de/04',
+				'/de/05',
+				'/de/06',
+				'/en/01',
+				'/en/02',
+				'/en/03',
+				'/en/04',
+				'/en/05',
+				'/en/06',
+				'/si/01',
+				'/si/02',
+				'/si/03',
+				'/si/04',
+				'/si/05',
+				'/si/06',
+				'/de',
+				'/en',
+				'/si'
+			]
+		},
 		paths: {
 			base : '',
 			relative: false
