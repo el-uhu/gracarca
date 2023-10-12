c02f694a478950e7e344f808f10edff6a6d44c81
Lukas Hutter <el-uhu@users.noreply.github.com>
2023-10-12 17:06:12 +0000

    added icons to cards

--- src/components/gracarca/Card.svelte
+++ src/components/gracarca/Card.svelte
@@ -4,15 +4,36 @@
     // console.log(article);
 </script>
 
-<div>
-    <h6>
-        <a href= "{article.slug}" target="_self"> {article.slug} {article.title}</a>
-    </h6>
-    <p>
-        {article.description}
-    </p>
+<div class="card-container">
+    <div class="icon-left">
+        <img src="/img/icons/{article.slug}.png"/>        
+    </div>
+    <div class="text-right">
+        <h6>
+            <a href= "{article.slug}" target="_self"> {article.slug} {article.title}</a>
+        </h6>
+        <p>
+            {article.description}
+        </p>
+    </div>
 </div>
 
 <style>
+    .card-container{
+        display: flex;
+    }
+
+    .icon-left{
+        display:flex;
+        justify-content: center;
+        width: 25%;
+        vertical-align: middle;
+        align-items: center;
+    }
+
+    .text-right{
+        width: 70%;
+        align-items: right;
+    }
 
 </style>
