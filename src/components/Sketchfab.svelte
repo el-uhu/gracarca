1c737b89b2f1e95eb7b3dcb1760a8986df2bbebe
Lukas Hutter <el-uhu@users.noreply.github.com>
2023-10-13 04:44:30 +0000

    sketchfab margin

--- src/components/Sketchfab.svelte
+++ src/components/Sketchfab.svelte
@@ -4,5 +4,12 @@
 </script>
 
 <div class="sketchfab-embed-wrapper"> 
-    <iframe title="Gracarca_SkyView_Weg_Tafeln_2023-03-22" frameborder="0" allowfullscreen mozallowfullscreen="true" webkitallowfullscreen="true" allow="autoplay; fullscreen; xr-spatial-tracking" xr-spatial-tracking execution-while-out-of-viewport execution-while-not-rendered web-share src="https://sketchfab.com/models/1b157d9bde36479a841b2018e5d89c69/embed?autostart=1&camera=0&annotations_visible=1&dnt=0&scrollwheel=0{camera}" width=100% height=500px> </iframe>
+    <iframe title="Gracarca_SkyView_Weg_Tafeln_2023-03-22" frameborder="0" allowfullscreen mozallowfullscreen="true" webkitallowfullscreen="true" allow="autoplay; fullscreen; xr-spatial-tracking" xr-spatial-tracking execution-while-out-of-viewport execution-while-not-rendered web-share src="https://sketchfab.com/models/1b157d9bde36479a841b2018e5d89c69/embed?autostart=1&camera=0&annotations_visible=1&dnt=0{camera}" width=100% height=500px> </iframe>
 </div>
+
+<style>
+    .sketchfab-embed-wrapper {
+        margin-left:1em;
+        margin-right: 1em;
+    }
+</style>
