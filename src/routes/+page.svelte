a74dfb175a2043db6c284a90f2b5629bceaf7e9d
Lukas Hutter <el-uhu@users.noreply.github.com>
2023-10-13 08:08:52 +0000

    fix incense embed and link target on landing page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,13 @@
 <div id="gracarca">
 	<section class="content-section">
 		<h6>
-			<a href="/en">View the page in English</a>
+			<a href="/en" target="_self">View the page in English</a>
 		</h6>
 		<h6>
-			<a href="/si">Oglejte si spletno stran v slovenščini</a>
+			<a href="/si" target="_self">Oglejte si spletno stran v slovenščini</a>
 		</h6>
 		<h6>
-			<a href="/de">Die Seite auf Deutsch anzeigen</a>
+			<a href="/de" target="_self">Die Seite auf Deutsch anzeigen</a>
 		</h6>
 	</section>
 </div>
